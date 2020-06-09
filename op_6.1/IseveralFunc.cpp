e7ce463ea2e5c4bec9449567806c4c6aec03acf8
Mitzengendler <54206076+Mitzengendler@users.noreply.github.com>
2020-06-09 13:03:08 +0300

    add expresions

--- op_6.1/IseveralFunc.cpp
+++ op_6.1/IseveralFunc.cpp
@@ -2,10 +2,10 @@
 
 double TwoVarFunc::calculate(double x, double y)
 {
-	return 0.0;
+	return pow(x, 2) + sqrt(3 * pow(y, 3));
 }
 
 double ThreeVarFunc::calculate(double x, double y, double z)
 {
-	return 0.0;
+	return 3 * pow(x, 2) * pow(z, 1/3) + pow(exp(1), sqrt(4 * y));
 }
